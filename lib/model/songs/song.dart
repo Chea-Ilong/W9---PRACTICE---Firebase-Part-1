76487f6ed1bcafb1784f80c9888111b779099b09
Chea Ilong <cheadara133@gmail.com>
2026-03-23 18:35:25 +0700

    W9-01 – Fetch and display songs in the library screen

--- lib/model/songs/song.dart
+++ lib/model/songs/song.dart
@@ -1,18 +1,18 @@
 class Song {
-  final String id;
   final String title;
-  final String artist;
   final Duration duration;
+  final String artistId;
+  final Uri imageUrl;
 
   Song({
-    required this.id,
     required this.title,
-    required this.artist,
     required this.duration,
-  });
+    required this.artistId,
+    required this.imageUrl,
+});
 
   @override
   String toString() {
-    return 'Song(id: $id, title: $title, artist: $artist, duration: $duration)';
+    return 'Song(title: $title, duration: $duration, , artist: $artistId )';
   }
 }
